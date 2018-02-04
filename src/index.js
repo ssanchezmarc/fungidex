393bc7f5feead4ee1f1b6cf5da5bf6ac45344950
Sergio Sánchez <sergiosanchezmarcos@gmail.com>
2018-02-04 20:31:24 +0100

    add css styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+
+import './css/bootstrap.css';
+import './css/index.css';
+
+import App from './containers/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
