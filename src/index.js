4c968aa18d6da9c18a04c858b06b63aa8f86ce29
Sergio Sánchez <sergiosanchezmarcos@gmail.com>
2018-02-04 21:20:09 +0100

    ad bootstrap into the index.html

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-import './css/bootstrap.css';
+// import './css/bootstrap.css';
 import './css/index.css';
 
 import App from './containers/App';
